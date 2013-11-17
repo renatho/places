f0bd1dd8bd84bd23349f8dbfb77c2f5dd6d26485
caroldarski <caroldarski@gmail.com>
2013-11-17 18:44:53 -0200

    Login validation
    (Validation started)

--- src/com/app/places/Login.java
+++ src/com/app/places/Login.java
@@ -1,8 +1,15 @@
 package com.app.places;
 
+import java.sql.ResultSet;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.OverlayItem;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.CursorJoiner.Result;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -28,21 +35,15 @@ public class Login extends Activity {
 		// Password
 		_password = (EditText) findViewById(R.id.txt_passw);
 
-		/*// Save
-		Button btnSave = (Button) findViewById(R.id.btnSave);
-		btnSave.setOnClickListener(new OnClickListener() {
-			public void onClick(View view) {
-				Log.d(TAG, "qwe qweqwuewuiegqw y gqwueyqw");
-			}
-		});*/
-
 		// Signout
 		Button btnLogin = (Button) findViewById(R.id.btn_login);
 		btnLogin.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
 				Log.d(TAG, "Login");
+				new LoginValidator().execute();
+				/*
 				Intent intent = new Intent(Login.this, MapViewActivity.class);
-				startActivity(intent);
+				startActivity(intent);*/
 			}
 		});
 		
@@ -51,9 +52,48 @@ public class Login extends Activity {
 		register.setOnClickListener(new OnClickListener(){
 			public void onClick(View view){
 				Log.d(TAG, "Register");
+				
 				Intent intent = new Intent(Login.this, ProfileActivity.class);
 				startActivity(intent);
 			}
 		});
+	}
+	
+	
+	public class LoginValidator extends AsyncTask<Integer, String, Integer> {
+
+		@Override
+		protected Integer doInBackground(Integer... params) {
+	    	String user = "'" + _username.getText().toString().toLowerCase() + "'";
+	    	String pass = "'" + _password.getText().toString().toLowerCase() + "'";
+	    	int count = 0;
+	    	// Chama dados do banco
+			DatabaseHelper db = new DatabaseHelper();
+
+			try {
+				db.open();
+				ResultSet rs = db.executeQuery("SELECT profile.id, profile.login, profile.password " +
+												" FROM profile" +
+												" WHERE profile.login = " + user + "  AND " +
+											    " profile.password = " + pass );
+				while (rs.next()) {
+					Log.d("Login", "count " + count);
+					 count++;
+				}				
+		    	
+		    	rs.close();
+				db.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return count;
+	    	
+		}
+		
+        @Override
+        protected void onPostExecute(Integer result) {
+        	Log.d("Login", "restul " + result);
+        }
+		
 	}
 }
